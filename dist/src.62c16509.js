50a7be9c496e5cd87d81f5a61c6d768a09c21d39
huyphung1602 <huy.phung.sw@gmail.com>
2022-04-11 13:04:32 +0700

    Fix yarn.lock

--- dist/src.62c16509.js
+++ dist/src.62c16509.js
@@ -28285,7 +28285,7 @@ if ("development" === 'production') {
 } else {
   module.exports = require('./cjs/react-dom.development.js');
 }
-},{"./cjs/react-dom.development.js":"../node_modules/react-dom/cjs/react-dom.development.js"}],"src/Elm/Main.elm":[function(require,module,exports) {
+},{"./cjs/react-dom.development.js":"../node_modules/react-dom/cjs/react-dom.development.js"}],"../src/Elm/Main.elm":[function(require,module,exports) {
 (function(scope){
 'use strict';
 
@@ -39507,7 +39507,7 @@ function ReactComponent(_a) {
 }
 
 react_dom_1.default.render(react_1.default.createElement(App, null), document.querySelector("#root"));
-},{"react":"../node_modules/react/index.js","react-dom":"../node_modules/react-dom/index.js","./Elm/Main":"src/Elm/Main.elm"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js","react-dom":"../node_modules/react-dom/index.js","./Elm/Main":"../src/Elm/Main.elm"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -39535,7 +39535,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44283" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39821" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
