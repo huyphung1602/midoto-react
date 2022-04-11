5fc11488b9fcd2b33c3b38028fe9ec5d9899b2e0
huyphung1602 <huy.phung.sw@gmail.com>
2022-04-11 12:50:45 +0700

    Reconfig for production

--- src/Elm/Main/index.d.ts
+++ src/Elm/Main/index.d.ts
@@ -1,19 +1,45 @@
-// WARNING: Do not manually modify this file. It was generated using:
-// https://github.com/dillonkearns/elm-typescript-interop
-// Type definitions for Elm ports
+export type JsonObject = { [Key in string]?: JsonValue };
+export type JsonArray = JsonValue[];
 
-export namespace Elm {
-  namespace Main {
-    export interface App {
-      ports: {
-        updateCountInReact: {
-          subscribe(callback: (data: number) => void): void
-        }
-      };
-    }
-    export function init(options: {
-      node?: HTMLElement | null;
-      flags: number;
-    }): Elm.Main.App;
-  }
+/**
+Matches any valid JSON value.
+Source: https://github.com/sindresorhus/type-fest/blob/master/source/basic.d.ts
+*/
+export type JsonValue =
+  | string
+  | number
+  | boolean
+  | null
+  | JsonObject
+  | JsonArray;
+
+export interface ElmApp {
+  ports: {
+    interopFromElm: PortFromElm<FromElm>;
+    interopToElm: PortToElm<ToElm>;
+    [key: string]: UnknownPort;
+  };
 }
+
+export type FromElm = { data : { message : string }; tag : "alert" };
+
+export type ToElm = { tag : "authenticatedUser"; username : string };
+
+export type Flags = null;
+
+export namespace Main {
+  function init(options: { node?: HTMLElement | null; flags: Flags }): ElmApp;
+}
+
+export as namespace Elm;
+
+export { Elm };
+
+export type UnknownPort = PortFromElm<unknown> | PortToElm<unknown> | undefined;
+
+export type PortFromElm<Data> = {
+  subscribe(callback: (fromElm: Data) => void): void;
+  unsubscribe(callback: (fromElm: Data) => void): void;
+};
+
+export type PortToElm<Data> = { send(data: Data): void };
